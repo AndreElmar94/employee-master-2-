45166d9a6c6bf5e0778c3e234ac54f7e1e4520cb
AndreElmar94 <atir.lapinskaya@gmail.com>
2021-04-21 15:22:36 +0300

    create entity/Employee.java

---
+++ src/main/java/com/mastery/java/task/entity/Employee.java
@@ -0,0 +1,36 @@
+package com.mastery.java.task.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int employeeId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "department_id")
+    private int departmentId;
+
+    @Column(name = "job_title")
+    private String jobTitle;
+
+    @Column(name = "date_of_birth")
+    private Date dateOfBirth;
+
+}
