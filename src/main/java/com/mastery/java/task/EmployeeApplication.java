bc7f316cf21f8661039a6ec886f3a2d13ebfd245
AndreElmar94 <atir.lapinskaya@gmail.com>
2021-04-21 15:21:44 +0300

    create EmployeeApplication.java

---
+++ src/main/java/com/mastery/java/task/EmployeeApplication.java
@@ -0,0 +1,15 @@
+package com.mastery.java.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeApplication {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        SpringApplication.run(EmployeeApplication.class, args);
+
+        Class.forName("org.postgresql.Driver");
+    }
+}
