e99f7eafa74f087a2d316401af89d94f923cee7d
AndreElmar94 <atir.lapinskaya@gmail.com>
2021-04-22 14:13:26 +0300

    add @Transactional in -> EmployeeServiceImpl.java

--- src/main/java/com/mastery/java/task/service/EmployeeServiceImpl.java
+++ src/main/java/com/mastery/java/task/service/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import com.mastery.java.task.entity.Employee;
 import com.mastery.java.task.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,21 +20,25 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Employee getById(int id) {
         return employeeRepository.getById(id);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Employee> findAll() throws Exception {
         return employeeRepository.findAll();
     }
 
     @Override
+    @Transactional
     public void update(Employee employee) throws Exception {
         employeeRepository.update(employee);
     }
 
     @Override
+    @Transactional
     public void delete(int id) throws Exception {
         employeeRepository.delete(id);
     }
