cf2b7b42caff73435ed6dadf200e11bb2fa99da3
AndreElmar94 <atir.lapinskaya@gmail.com>
2021-05-02 16:48:04 +0300

    update Employee.java

--- src/main/java/com/mastery/java/task/entity/Employee.java
+++ src/main/java/com/mastery/java/task/entity/Employee.java
@@ -1,36 +1,23 @@
 package com.mastery.java.task.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
 import java.sql.Date;
 
 @Data
-@Entity
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "employee")
 public class Employee {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int employeeId;
-
-    @Column(name = "first_name")
     private String firstName;
-
-    @Column(name = "last_name")
     private String lastName;
-
-    @Column(name = "department_id")
     private int departmentId;
-
-    @Column(name = "job_title")
     private String jobTitle;
-
-    @Column(name = "date_of_birth")
     private Date dateOfBirth;
 
 }
